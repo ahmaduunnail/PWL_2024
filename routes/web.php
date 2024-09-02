ee52c0bd679abc2bf93c6496530f06d54e3e754b
ahmaduunnail <ahmadunnail0@gmail.com>
2024-09-02 09:55:44 +0700

    🐋 Praktikum-1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Selamat Datang";
+});
+
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return "NIM: 2241760071
+            <br>
+            Nama: Ahmadun Na'il";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
+
+Route::get('/user/{name?}', function ($name = "John") {
+    return 'Nama saya ' . $name;
 });
