883d84a025fbaf84c7b2bb2ba10d0a575c47f90d
ahmaduunnail <ahmadunnail0@gmail.com>
2024-09-02 10:40:11 +0700

    🐋 Praktikum-2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about()
+    {
+        return "Nama: Ahmadun Na'il 
+                <br>
+                NIM: 2241760071";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,25 +18,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Selamat Datang";
-});
+Route::get('/', [HomeController::class, 'index']);
 
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/about', function () {
-    return "NIM: 2241760071
-            <br>
-            Nama: Ahmadun Na'il";
-});
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
@@ -41,9 +51,6 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID " . $id;
-});
 
 Route::get('/user/{name?}', function ($name = "John") {
     return 'Nama saya ' . $name;
